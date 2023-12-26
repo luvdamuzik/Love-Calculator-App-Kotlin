d41062b57b5b11bc41437cf23a3c009a0f64683e
Rijad Mutapcic <134503577+luvdamuzik@users.noreply.github.com>
2023-12-26 10:54:41 +0100

    Update Constants.kt

--- app/src/main/java/com/example/android/navigation/Constants.kt
+++ app/src/main/java/com/example/android/navigation/Constants.kt
@@ -1,9 +1,9 @@
 package com.example.android.navigation
 
-const val API_KEY_USERS = "2592b3340emsh727791bed09ba5dp1467acjsn8b45b9a54eb8"
+const val API_KEY_USERS = ""
 const val HOST_USERS = "random-user.p.rapidapi.com"
 const val BASE_URL_USERS = "https://random-user.p.rapidapi.com"
-const val API_KEY_LOVE = "9d7ed97ad9mshb02e4c2446c9fe9p1ed301jsn6b5089c559e1"
+const val API_KEY_LOVE = ""
 const val HOST_LOVE= "love-calculator.p.rapidapi.com"
 const val BASE_URL_LOVE= "https://love-calculator.p.rapidapi.com"
 var FIRST_NAME = ""
